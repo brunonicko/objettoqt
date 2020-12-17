e34ac1f48f7b91882333ca713acdda8d65c78074
Bruno Nicko <brunonicko@gmail.com>
2020-12-17 15:23:53 -0800

    Fix documentation build for readthedocs

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r") as fh:
 
 setuptools.setup(
     name="objettoqt",
-    version="0.1.0",
+    version="0.1.1",
     author="Bruno Nicko",
     author_email="brunonicko@gmail.com",
     description="Utilities to use Objetto with Qt",
